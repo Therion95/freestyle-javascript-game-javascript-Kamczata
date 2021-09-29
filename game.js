f470fa09e606b99e9ab4d130dd0fd4cf4d8465ed
Kamczata <kam.czernek@gmail.com>
2021-09-29 21:34:57 +0200

    kamila changes

--- game.js
+++ game.js
@@ -1,29 +1,29 @@
-// console.log("Hello world!");
-//
-//
-//
-// initGame();
-//
-// function initGame() {
-//
-//     // Your game can start here, but define separate functions, don't write everything in here 🙂
-//     console.log("cokolwiek");
-//     let aaa = "aaa";
-//     console.log(aaa);
-//     console.log("cokolwiek");
-//     let gameLevels = document.querySelectorAll(".data-game-level");
-//     console.log(gameLevels);
-//
-//     for (let gameLevel of gameLevels) {
-//         gameLevel.addEventListener("click", initGameHandler);
-//     }
-//
-//
-//     function initGameHandler() {
-//         alert("Hellooooo");
-//         console.log("coookolwiek");
-//     }
-//
-// }
-//
+console.log("Hello world!");
+
+
+
+initGame();
+
+function initGame() {
+
+    // Your game can start here, but define separate functions, don't write everything in here 🙂
+    console.log("cokolwiek");
+    let aaa = "aaa";
+    console.log(aaa);
+    console.log("cokolwiek");
+    let gameLevels = document.querySelectorAll(".data-game-level");
+    console.log(gameLevels);
+
+    for (let gameLevel of gameLevels) {
+        gameLevel.addEventListener("click", initGameHandler);
+    }
+
+
+    function initGameHandler() {
+        alert("Hellooooo");
+        console.log("coookolwiek");
+    }
+
+}
+
 
